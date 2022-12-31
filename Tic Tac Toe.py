abbcf287bcb9c656b493f2921d50722ad90fecb8
Loqman Adnane <loqman.adnane@outlook.com>
2022-12-30 19:19:34 -0500

    row1 call fail

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -6,8 +6,10 @@ row3 = ['7','|','8','|','9']
 string1 = ''
 string2 = ''
 string3 = ''
-PlayerTurn = 1
-Turn = 0
+playerTurn = 1
+turn = 0
+options = [1,2,3,4,5,6,7,8,9]
+choice = 0
 def display(row1, string1, sep1, row2, string2, sep2, row3, string3):
     for x in row1:
         string1 += x
@@ -21,20 +23,22 @@ def display(row1, string1, sep1, row2, string2, sep2, row3, string3):
     print(sep2)
     print(string3)
 def UserChoice():
-    choice = input('Please enter a value corresponding to the available spots in the chart to place your mark (1-9): ')
-    return(int(choice))
-while Turn < 10:
-    if PlayerTurn == 1:
+    choice = (input('Please enter a value corresponding to the available spots in the chart to place your mark (1-9): '))
+    return(choice)
+while turn < 10:
+    if playerTurn == 1:
         print(display(row1, string1, sep1, row2, string2, sep2, row3, string3))
         print('Player 1:')
         UserChoice()
+        print(row1(choice))
         print(display(row1, string1, sep1, row2, string2, sep2, row3, string3))
-        PlayerTurn += 1
-        Turn += 1
-    if PlayerTurn == 2:
+        playerTurn += 1
+        turn += 1
+    if playerTurn == 2:
         print(display(row1, string1, sep1, row2, string2, sep2, row3, string3))
         print('Player 2:')
         UserChoice()
+        print(row1(choice))
         print(display(row1, string1, sep1, row2, string2, sep2, row3, string3))
-        PlayerTurn -= 1
-        Turn += 1
+        playerTurn -= 1
+        turn += 1
